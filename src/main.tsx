d198d4392cba13b862400fce4804cce91d8aaaff
Kim Joseph Peñaloza <kj.penaloza@gmail.com>
2025-12-08 13:46:49 +0800

    refactor: Add @mui/x-license dependency and set license key in main.tsx; remove unused CSS files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { LicenseInfo } from '@mui/x-license';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.tsx';
-import { persistor, store } from './store/index.tsx';
+import { persistor, store } from '@/store/index.tsx';
+
+LicenseInfo.setLicenseKey(import.meta.env.VITE_MUI_LICENSE_KEY);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
